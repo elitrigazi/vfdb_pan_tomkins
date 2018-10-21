2519ca05ba873cbd065122a3a668c48fda726e4a
Elisavet Trigazi <elitrigazi@gmail.com>
2018-10-21 13:29:18 -0400

    Reduce categories to 5
    AFIB
    B-VT-VF-VFL-ASYS
    BI
    HGEA-N-NOD-PM-SBR-VER
    SVTA
    
    Signed-off-by: Elisavet Trigazi <elitrigazi@gmail.com>

---
+++ create_five_categories.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cp vfdb-qrs/segments.arff vfdb-qrs/segments-5-classes.arff 
+sed -e 's/,HGEA$/,HGEA-N-NOD-PM-SBR-VER/g' vfdb-qrs/segments-5-classes.arff | grep HGEA-N-NOD-PM-SBR-VER | wc -l
+sed -e 's/,N$/,HGEA-N-NOD-PM-SBR-VER/g'    vfdb-qrs/segments-5-classes.arff | grep HGEA-N-NOD-PM-SBR-VER | wc -l
+sed -e 's/,NOD$/,HGEA-N-NOD-PM-SBR-VER/g'  vfdb-qrs/segments-5-classes.arff | grep HGEA-N-NOD-PM-SBR-VER | wc -l
+sed -e 's/,PM$/,HGEA-N-NOD-PM-SBR-VER/g'   vfdb-qrs/segments-5-classes.arff | grep HGEA-N-NOD-PM-SBR-VER | wc -l
+sed -e 's/,SBR$/,HGEA-N-NOD-PM-SBR-VER/g'  vfdb-qrs/segments-5-classes.arff | grep HGEA-N-NOD-PM-SBR-VER | wc -l
+sed -e 's/,VER$/,HGEA-N-NOD-PM-SBR-VER/g'  vfdb-qrs/segments-5-classes.arff | grep HGEA-N-NOD-PM-SBR-VER | wc -l
+
+sed -i -e 's/,ASYS$/,B-VT-VF-VFL-ASYS/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,B$/,B-VT-VF-VFL-ASYS/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,VT$/,B-VT-VF-VFL-ASYS/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,VF$/,B-VT-VF-VFL-ASYS/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,VFL$/,B-VT-VF-VFL-ASYS/g' vfdb-qrs/segments-5-classes.arff
+
+sed -i -e 's/,HGEA$/,HGEA-N-NOD-PM-SBR-VER/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,N$/,HGEA-N-NOD-PM-SBR-VER/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,NOD$/,HGEA-N-NOD-PM-SBR-VER/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,PM$/,HGEA-N-NOD-PM-SBR-VER/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,SBR$/,HGEA-N-NOD-PM-SBR-VER/g' vfdb-qrs/segments-5-classes.arff
+sed -i -e 's/,VER$/,HGEA-N-NOD-PM-SBR-VER/g' vfdb-qrs/segments-5-classes.arff
+
+ASYS=$(grep ",ASYS$" vfdb-qrs/segments.arff | wc -l)
+B=$(grep ",B$" vfdb-qrs/segments.arff | wc -l)
+VT=$(grep ",VT$" vfdb-qrs/segments.arff | wc -l)
+VF=$(grep ",VF$" vfdb-qrs/segments.arff | wc -l)
+VFL=$(grep ",VFL$" vfdb-qrs/segments.arff | wc -l)
+
+echo "---"
+res=$(($ASYS + $B + $VT + $VF + $VFL))
+echo $res
+grep ",B-VT-VF-VFL-ASYS$" vfdb-qrs/segments-5-classes.arff  | wc -l
+echo "---"
+
+
+HGEA=$(grep ",HGEA$" vfdb-qrs/segments.arff | wc -l)
+N=$(grep ",N$" vfdb-qrs/segments.arff | wc -l)
+NOD=$(grep ",NOD$" vfdb-qrs/segments.arff | wc -l)
+PM=$(grep ",PM$" vfdb-qrs/segments.arff | wc -l)
+SBR=$(grep ",SBR$" vfdb-qrs/segments.arff | wc -l)
+VER=$(grep ",VER$" vfdb-qrs/segments.arff | wc -l)
+echo "---"
+echo $HGEA
+echo $N
+echo $NOD
+echo $PM
+echo $SBR
+echo $VER
+echo "---"
+
+res=$(($HGEA + $N + $NOD + $PM + $SBR + $VER))
+echo $res
+grep ",HGEA-N-NOD-PM-SBR-VER$" vfdb-qrs/segments-5-classes.arff  | wc -l
+
+
